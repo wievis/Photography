26d63fdf989b347da72196cd51a7505f6cdf0f91
“tmczs” <tmczs@proton.me>
2026-01-25 08:34:34 +0100

    podglad miniaturek admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,17 +1,21 @@
 from django.contrib import admin
 from django.forms import TextInput, NumberInput
 from django.db import models
+from django.utils.html import format_html
 from .models import Photo, Visitor
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ['id', 'order', 'created']
+    list_display = ['id', 'display_thumbnail', 'order', 'created']
     list_editable = ['order']
     readonly_fields = ['created']
     formfield_overrides = {
         models.CharField: {'widget': TextInput(attrs={'size': '15'})},
-        models.IntegerField: {'widget': NumberInput(attrs={'size': '5'})},
+        models.IntegerField: {'widget': NumberInput(attrs={'style': 'width: 60px'})},
     }
+
+    def display_thumbnail(self, obj):
+        return format_html('<img src="{}" style="max-height: 100px; width: auto;" />', obj.thumbnail.url)
 
 @admin.register(Visitor)
 class VisitorAdmin(admin.ModelAdmin):
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from .models import Photo
 
 def home(request, lang='pl'):
-    return render(request, 'home.html', {'photos': Photo.objects.all(), 'lang': lang})
+    return render(request, 'home.html', {'photos': Photo.objects.order_by('order', '-created'), 'lang': lang})
 
 def galeria(request, lang='pl'):
-    return render(request, 'galeria.html', {'photos': Photo.objects.all(), 'lang': lang})
+    return render(request, 'galeria.html', {'photos': Photo.objects.order_by('order', '-created'), 'lang': lang})
 
 def kontakt(request, lang='pl'):
     return render(request, 'kontakt.html', {'lang': lang})
