0905c44d301107ed0981c24af9068335a528601d
“tmczs” <tmczs@proton.me>
2026-01-25 13:22:41 +0100

    zdublowane staticfiles_dirs blad

--- photography_portfolio_page/settings.py
+++ photography_portfolio_page/settings.py
@@ -66,9 +66,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio', 'static'),
-]
+STATICFILES_DIRS = []
 
 STORAGES = {
     "default": {
