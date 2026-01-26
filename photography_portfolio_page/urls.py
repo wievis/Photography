16f204a8c70e3b1b622d386d4dbadd5d2d3b050f
“tmczs” <tmczs@proton.me>
2026-01-26 20:30:11 +0100

    google favicon

--- photography_portfolio_page/urls.py
+++ photography_portfolio_page/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/svg/aparat.svg')),
     path('', include('portfolio.urls')),
 ]
 
